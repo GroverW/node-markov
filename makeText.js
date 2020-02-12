df0a5097d1b1d6faceb3fd67588e9750afcc8961
GroverW <cheesemaker@gmail.com>
2020-02-11 16:38:42 -0800

    Created makeText. Reads from file or url

--- makeText.js
+++ makeText.js
@@ -1,2 +1,34 @@
 /** Command-line tool to generate Markov text. */
+const { MarkovMachine } = require('./markov');
+const fs = require('fs');
+const axios = require('axios');
 
+if(process.argv.indexOf('file') !== -1) {
+  markovFromFile();
+} else {
+  markovFromURL();
+}
+
+function markovFromFile() {
+  fs.readFile(process.argv[3], 'utf8', function (err, data) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      };
+      let m = new MarkovMachine(data);
+      console.log(m.makeText());
+  });
+}
+
+async function markovFromURL() {
+  try {
+    response = await axios.get(process.argv[3])
+
+    let m = new MarkovMachine(response.data);
+    console.log(m.makeText())
+  } catch(error) {
+    console.error(error);
+    process.exit(1);
+  }
+  
+}
